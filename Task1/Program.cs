7617b1ea5a085943c7ee50448c0fe1651da1585c
Konstantin Chern <codahl@yandex.ru>
2023-03-09 11:47:21 +0300

    Task1 Solved

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,56 @@
 ﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочивает
 //  по убыванию элементы каждой строки двумерного массива.
 
+Console.Clear();
+
+int[,] CreatMatrix(int row, int col)
+{
+    int[,] array = new int[row, col]; // Создаем 2-мерный массив
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(-9, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+    }
+    Console.WriteLine();
+}
+
+void ColumnsRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] MyArray = CreatMatrix(4, 4);
+PrintArray(MyArray);
+ColumnsRows(MyArray);
+PrintArray(MyArray);
+
